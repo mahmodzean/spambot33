9febdc5404f6096fc5430f0c16b9fb3667aaba58
mahmodzean <44239884+mahmodzean@users.noreply.github.com>
2018-11-17 01:42:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,22 @@
-const Discord = require('discord.js');
+ const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("500557063991328770")
+let channel =     client.channels.get("513023117006995471")
 setInterval(function() {
 channel.send(`mmmmmmmmmmmmmmmmmmmmmmmmm mahmod pro xd pro xdxdxdxdxdxdxdxdxd`);
 }, 30)
 })
 
-client.login(process.env.BOT_TOKEN);
+
+client.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == ".8s") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+client.login(process.env.BOT_TOKEN); 
